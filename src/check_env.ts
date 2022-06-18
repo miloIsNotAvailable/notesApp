fea4640e04ba6a8be542bbcc47bd03129e5606d0
unknown <milosz20031207@gmail.com>
2022-06-18 17:49:05 +0200

    fixing cookies

--- src/check_env.ts
+++ src/check_env.ts
@@ -1,3 +1,3 @@
 const node_env: any = process.env.NODE_ENV
 
-export const check_env = node_env && node_env === "PRODUCTION" ? "https://notes-app-three-beta.vercel.app" : 'http://localhost:4000'
+export const check_env = node_env && node_env === "production" ? "https://notes-app-three-beta.vercel.app" : 'http://localhost:4000'
