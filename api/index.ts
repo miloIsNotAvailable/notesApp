62e51204d202f40b8571bdb0a3ee7d4b9cea3f9a
unknown <milosz20031207@gmail.com>
2022-06-18 20:15:36 +0200

    fixing everything

--- api/index.ts
+++ api/index.ts
@@ -63,7 +63,7 @@ const publicPath = path.join( __dirname, '..' )
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
+  res.sendFile(path.join(publicPath, 'roothtml.html'));
 });
 
 // create application/json parser
