7c5e408992eda78005157bfd799ec27e21621812
unknown <milosz20031207@gmail.com>
2022-06-21 23:28:53 +0200

    cleaned up graphql stuff a bit, fixed submit button showing when there is no content

--- api/graphql/schema.ts
+++ api/graphql/schema.ts
@@ -4,6 +4,6 @@ import { buildSchema } from 'graphql';
 /**
  * read file from ./graphql.schema
  */
-export const file = fs.readFileSync( './graphql/schema.graphql', 'utf8' )
+export const file = fs.readFileSync( './graphql/graphqlschema.graphql', 'utf8' )
 // Construct a schema, using GraphQL schema language
 export const schema = buildSchema( file );
