75ece09a6269879ffc60e3abc08721ab79d4708a
unknown <milosz20031207@gmail.com>
2022-06-23 22:20:39 +0200

    created notes get saved to the database

--- api/dbinterfaces.ts
+++ api/dbinterfaces.ts
@@ -1,9 +1,3 @@
 export type Funkee = { id: string,name: string }
 
 export type Note = { userId: string,title: string,content: string,type: string }
-
-type Note = { id: string,content: string,type: string,title: string,users: string }
-
-type Note = { id: string,content: string,type: string,title: string,users: string }
-
-type Note = { id: string,content: string,type: string,title: string,users: string }
