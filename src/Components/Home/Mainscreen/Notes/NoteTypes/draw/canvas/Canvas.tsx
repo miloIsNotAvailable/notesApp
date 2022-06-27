897ba8d0512e198456361b0847e693089e0d5e23
unknown <milosz20031207@gmail.com>
2022-06-27 20:56:29 +0200

    added basic functionality to canvas

--- src/Components/Home/Mainscreen/Notes/NoteTypes/draw/canvas/Canvas.tsx
+++ src/Components/Home/Mainscreen/Notes/NoteTypes/draw/canvas/Canvas.tsx
@@ -1,9 +1,52 @@
-import { FC } from "react";
+import { FC, MouseEvent, useEffect, useRef, useState } from "react";
 import { styles } from "../build/NoteCanvasStyles";
 
 const Canvas: FC = () => {
 
-    return <canvas id="canvas" className={ styles.canvas }/>
+    const [coords, setCoords] = useState<{ x: number, y: number }>( { x: 0, y: 0 } )
+    const [mouseDown, setMouseDown] = useState<boolean>( false )
+    const canvasRef = useRef<HTMLCanvasElement | null>( null )
+
+    useEffect(() => {
+        if( !canvasRef.current ) return
+        const canvas = canvasRef.current
+        const context = canvas.getContext('2d')
+        //Our first draw
+        if( !context ) return
+        context.fillStyle = 'white'
+        context.beginPath();
+    
+        context.lineWidth = coords.y * .1 * Math.sin( Math.random() * Math.PI * 2 );
+         
+        // Sets the end of the lines drawn
+        // to a round shape.
+        context.lineCap = 'round';
+          
+        context.strokeStyle = 'white';
+            
+        // The cursor to start drawing
+        // moves to this coordinate
+        context.moveTo(coords.x, coords.y);
+         
+        // A line is traced from start
+        // coordinate to this coordinate
+        context.lineTo(coords.x, coords.y);
+          
+        // Draws the line.
+        context.stroke();
+
+      }, [ coords ])
+
+    return <canvas 
+            id="canvas"
+            width={ window.innerWidth }
+            height={ window.innerHeight }
+            ref={ canvasRef }
+            className={ styles.canvas }
+            onMouseDown={ ( ) => setMouseDown( true ) }
+            onMouseMove={ ( { pageX, pageY } ) => mouseDown && setCoords( { x: pageX, y: pageY } ) }
+            onMouseUp={ ( ) => setMouseDown( false ) }
+        />
     }
 
 export default Canvas
