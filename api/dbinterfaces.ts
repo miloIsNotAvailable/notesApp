eaeffe784bec98668a4d1be4dbb40365489e923a
unknown <milosz20031207@gmail.com>
2022-06-30 15:05:16 +0200

    fixed new notes being displayed twice

--- api/dbinterfaces.ts
+++ api/dbinterfaces.ts
@@ -1,11 +1,3 @@
 export type Funkee = { id: string,name: string }
 
 export type Note = { id: string,users: string,title: string,content: string,type: string }
-
-type Note = { id: string,content: string,type: string,title: string,users: string }
-
-type Note = { id: string,content: string,type: string,title: string,users: string }
-
-type Note = { id: string,content: string,type: string,title: string,users: string }
-
-type Note = { id: string,content: string,type: string,title: string,users: string }
